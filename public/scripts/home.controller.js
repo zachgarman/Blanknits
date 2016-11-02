366f19184320d0c1215c7c589dcc972105fd9286
Zach Garman <zachgarman@gmail.com>
2016-11-01 20:56:31 -0500

    form clears

--- public/scripts/home.controller.js
+++ public/scripts/home.controller.js
@@ -6,8 +6,19 @@ function HomeController() {
 
   var home = this;
 
+  // Handle home screen 'request for more info' form.
   home.requestInfo = function() {
-    console.log('You clicked the Send button!');
+    var contact = {
+      name: home.name,
+      email: home.email,
+      phoneNumber: home.phoneNumber,
+      message: home.message,
+    };
+    // Send info in an email and as a post req to admin bucket
+    console.log(contact);
+    home.name = '';
+    home.email = '';
+    home.phoneNumber = '';
+    home.message = '';
   };
-
 }
